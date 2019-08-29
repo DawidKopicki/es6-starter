01180b1625151d9910984d6356c43c2fc72aedf1
DawidKopicki <dawidkopicki@gmail.com>
2019-08-29 14:29:36 +0200

    Destructuring

--- script-compiled.js
+++ script-compiled.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function sumOfTwoNumbers() {
     var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
@@ -8,3 +8,31 @@ function sumOfTwoNumbers() {
 }
 
 console.log(sumOfTwoNumbers());
+
+// l. 2
+
+var name = 'Main header';
+
+console.log(name.includes('header'));
+
+// Destructuring
+var obj = {
+    login: "Dawid",
+    age: 26
+};
+
+var login = obj.login,
+    age = obj.age;
+
+
+console.log(login, age);
+
+// Array destructuring
+
+var arr = [1, 2, 3, 4, 5];
+
+var a = arr[0],
+    b = arr[1],
+    c = arr[2];
+
+console.log(a + " " + b + " " + c);
--- script.js
+++ script.js
@@ -9,3 +9,20 @@ console.log(sumOfTwoNumbers());
 let name = 'Main header';
 
 console.log(name.includes('header'));
+
+// Destructuring
+const obj = {
+    login: "Dawid",
+    age: 26
+};
+
+const { login, age } = obj;
+
+console.log(login, age);
+
+// Array destructuring
+
+const arr = [1, 2, 3, 4, 5];
+
+const [ a, b, c ] = arr;
+console.log(a + " " + b + " " + c);
