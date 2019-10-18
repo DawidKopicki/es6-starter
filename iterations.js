9896a492413132fc7b0f009ccb41e0ec640cda43
DawidKopicki <42410702+DawidKopicki@users.noreply.github.com>
2019-10-18 11:05:34 +0200

    Iterations

---
+++ iterations.js
@@ -0,0 +1,14 @@
+let table = ['Adam', 'Tomek', 'Laura', 'Ola'];
+
+// for(const key in table) {
+//     console.log(key + ' ' + table[key]);
+// }
+
+for(const value of table) {
+    console.log(value);
+}
+
+const name = 'Dawid';
+for(const char of name) {
+    console.log(char);
+}
