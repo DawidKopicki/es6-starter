81c6c3af633d07414497571e17f06030cc9690d9
DawidKopicki <42410702+DawidKopicki@users.noreply.github.com>
2019-10-18 11:11:16 +0200

    Own iterator

---
+++ own-iterator.js
@@ -0,0 +1,18 @@
+function createIterator(array) {
+    let nextIndex = 0;
+
+    return {
+        next: function () {
+            return nextIndex < array.length ?
+                { value: array[nextIndex++], done: false } :
+                { done: true };
+        }
+    };
+}
+
+let users = createIterator(['Andrzej', 'Paweł', "Lucas"]);
+
+console.log(users.next().value);
+console.log(users.next().value);
+console.log(users.next().value);
+console.log(users.next().done);
