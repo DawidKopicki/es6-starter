8bd3f33ecf7d03eb6f5bd48319828559df88a72e
DawidKopicki <dawidkopicki@gmail.com>
2019-10-29 10:42:38 +0100

    Math Object methods

---
+++ math-obj-methods.js
@@ -0,0 +1,11 @@
+const m = 2.453;
+console.log(Math.trunc(m));
+
+const n = -23;
+console.log(Math.sign(n)); // returns -1 (or 0 for 0, +1 for +numbers, NaN)
+
+const l = 8;
+console.log(Math.log2(l)); // returns 2
+
+const s = 100;
+console.log(Math.log10(s));
